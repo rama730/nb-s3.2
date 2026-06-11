2724714f403d008b4e181fe0015f8f6c7b854c8c
CH Rama <217795742+rama730@users.noreply.github.com>
2026-06-12 00:49:28 +0530

    readme: add readme-settings.ts
    File: src/lib/projects/readme-settings.ts

---
+++ src/lib/projects/readme-settings.ts
@@ -0,0 +1,10 @@
+export {
+    DEFAULT_PROJECT_README_SETTINGS,
+    normalizeProjectReadmeSettings,
+    resolveProjectReadmePermission,
+    type ProjectReadmeEditPolicy,
+    type ProjectReadmePermission,
+    type ProjectReadmePublicVisibility,
+    type ProjectReadmeSettings,
+} from "@/lib/projects/readme";
+
