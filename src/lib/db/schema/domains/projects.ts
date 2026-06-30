8a89942301c3421b89a21cc2389fe0ca4e041cc2
CH Rama <217795742+rama730@users.noreply.github.com>
2026-06-30 09:46:45 +0530

    chore: add src/lib/db/schema/domains/projects.ts

---
+++ src/lib/db/schema/domains/projects.ts
@@ -0,0 +1,19 @@
+export {
+  projectFollows,
+  projectMarkdownAssets,
+  projectMarkdownDraftContributors,
+  projectMarkdowns,
+  projectMarkdownVersions,
+  projectMembers,
+  projectOpenRoles,
+  projectSprints,
+  projectUpdateComments,
+  projectUpdateDrafts,
+  projectUpdateLikes,
+  projectUpdates,
+  projects,
+  roleApplications,
+  savedProjects,
+  tasks,
+} from "../index";
+
