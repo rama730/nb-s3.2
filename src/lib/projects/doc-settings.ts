40032ab35494ee40ddb21f3c7b4889c889b351f1
CH Rama <217795742+rama730@users.noreply.github.com>
2026-06-30 09:46:50 +0530

    chore: add src/lib/projects/doc-settings.ts

---
+++ src/lib/projects/doc-settings.ts
@@ -0,0 +1,10 @@
+export {
+    DEFAULT_PROJECT_DOC_SETTINGS,
+    normalizeProjectDocSettings,
+    resolveProjectDocPermission,
+    type ProjectDocEditPolicy,
+    type ProjectDocPermission,
+    type ProjectDocPublicVisibility,
+    type ProjectDocSettings,
+} from "@/lib/projects/doc";
+
