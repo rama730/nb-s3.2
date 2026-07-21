73496c646d7bd2d0ddb960f12b205c7127b76752
CH Rama <217795742+rama730@users.noreply.github.com>
2026-07-21 23:46:13 +0530

    chore: remove src/lib/runner/javascript.ts

--- src/lib/runner/javascript.ts
+++
@@ -1,6 +0,0 @@
-import type { ExecutorResult } from "./types";
-import { runInBrowserSandbox } from "./browser-sandbox";
-
-export async function runJavaScript(code: string): Promise<ExecutorResult> {
-  return runInBrowserSandbox(code);
-}
