3ef7a990f6faa64940bbcf46631bc5ea3259fd47
CH Rama <217795742+rama730@users.noreply.github.com>
2026-07-21 23:44:55 +0530

    chore: update src/app/actions/files/index.ts

--- src/app/actions/files/index.ts
+++ src/app/actions/files/index.ts
@@ -3,7 +3,6 @@ export * from './nodes';
 export * from './mutations';
 export * from './content';
 export * from './search';
-export * from './locks';
 export * from './links';
 export * from './events';
 export * from './versions';
